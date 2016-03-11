4057775a0106c2ce13d27cf92eb9870cacf8114d
Individual IT Services <info@individual-it.net>
2016-03-11 15:49:30 +0545

    initial upload

---
+++ ntfs-hardlink-backup/backup-reminder/backup-reminder-text.ps1
@@ -0,0 +1,5 @@
+$text=@{
+	'no status file'="No Backup was set up in your computer`nPlease contact ICT by writing an email to 'ict@nepal.inf.org'";
+	'last backup failed'="Your last backup did fail`nPlease contact ICT by 'ict@nepal.inf.org'";
+	'backup to old'="You have not done a backup for {0} days. Please run your backup!"
+	}
